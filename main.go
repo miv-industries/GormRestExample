4797f6cad6b6bd7a55a1e6c7bc1b7c30486f3860
miv-industries <miv@mivdv.net>
2022-06-09 00:48:55 +0300

    added get single order endpoint

--- main.go
+++ main.go
@@ -17,21 +17,23 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api", welcome)
 
 	// User endpoints
-	app.Post("api/users", routes.CreateUser)
-	app.Get("api/users", routes.GetUsers)
-	app.Get("api/users/:id", routes.GetUser)
-	app.Put("api/users/:id", routes.UpdateUser)
-	app.Delete("api/users/:id", routes.DeleteUser)
+	app.Post("/api/users", routes.CreateUser)
+	app.Get("/api/users", routes.GetUsers)
+	app.Get("/api/users/:id", routes.GetUser)
+	app.Put("/api/users/:id", routes.UpdateUser)
+	app.Delete("/api/users/:id", routes.DeleteUser)
 
 	// Product endpoints
-	app.Post("api/products", routes.CreateProduct)
-	app.Get("api/products", routes.GetProducts)
-	app.Get("api/products/:id", routes.GetProduct)
+	app.Post("/api/products", routes.CreateProduct)
+	app.Get("/api/products", routes.GetProducts)
+	app.Get("/api/products/:id", routes.GetProduct)
 	app.Put("api/products/:id", routes.UpdateProduct)
-	app.Delete("api/products/:id", routes.DeleteProduct)
+	app.Delete("/api/products/:id", routes.DeleteProduct)
 
 	// Order endpoints
-	app.Post("api/orders", routes.CreateOrder)
+	app.Post("/api/orders", routes.CreateOrder)
+	app.Get("/api/orders", routes.GetOrders)
+	app.Get("/api/orders/:id", routes.GetOrder)
 }
 
 func main() {
--- routes/order.go
+++ routes/order.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"errors"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -33,6 +34,14 @@ func CreateResponseOrder(order models.Order, user User, product Product) Order {
 	return Order{ID: order.ID, User: user, Product: product, CreatedAt: order.CreatedAt}
 }
 
+func findOrder(id int, order *models.Order) error {
+	database.Database.Db.Find(&order, "id = ?", id)
+	if order.ID == 0 {
+		return errors.New("Order does not exist")
+	}
+
+	return nil
+}
 func CreateOrder(c *fiber.Ctx) error {
 
 	// we check for field errors here and we parse the order into the var
@@ -48,7 +57,7 @@ func CreateOrder(c *fiber.Ctx) error {
 
 	// we check that the product foreign key object actually exists and place it in the var
 	var product models.Product
-	if err := findProduct(order.UserRefer, &product); err != nil {
+	if err := findProduct(order.ProductRefer, &product); err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
 
@@ -58,6 +67,50 @@ func CreateOrder(c *fiber.Ctx) error {
 	// these create functions basically use a struct type as a serializer for the response field or nested object
 	responseUser := CreateResponseUser(user)
 	responseProduct := CreateResponseProduct(product)
+	responseOrder := CreateResponseOrder(order, responseUser, responseProduct)
+
+	return c.Status(200).JSON(responseOrder)
+}
+
+func GetOrders(c *fiber.Ctx) error {
+	orders := []models.Order{}
+	database.Database.Db.Find(&orders)
+	responseOrders := []Order{}
+
+	for _, order := range orders {
+		var user models.User
+		var product models.Product
+		database.Database.Db.Find(&user, "id = ?", order.UserRefer)
+		database.Database.Db.Find(&product, "id = ?", order.ProductRefer)
+
+		responseOrder := CreateResponseOrder(order, CreateResponseUser(user), CreateResponseProduct(product))
+		responseOrders = append(responseOrders, responseOrder)
+
+	}
+	return c.Status(200).JSON(responseOrders)
+
+}
+
+func GetOrder(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	var order models.Order
+
+	if err != nil {
+		return c.Status(400).JSON("Please ensure that :id is an integer")
+	}
+
+	if err := findOrder(id, &order); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	var user models.User
+	var product models.Product
+
+	database.Database.Db.First(&user, order.UserRefer)
+	database.Database.Db.First(&product, order.ProductRefer)
+	responseUser := CreateResponseUser(user)
+	responseProduct := CreateResponseProduct(product)
+
 	responseOrder := CreateResponseOrder(order, responseUser, responseProduct)
 
 	return c.Status(200).JSON(responseOrder)
